c6954b5eff16e27a404fbe2736dd9a2b599b2f03
amephraim <ame378@nyu.edu>
2014-04-08 13:14:14 -0400

    added South for database migration

--- django_project/django_project/settings.py
+++ django_project/django_project/settings.py
@@ -36,6 +36,10 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+#Third part apps
+    'south',
+    
 )
 
 MIDDLEWARE_CLASSES = (
